d22c0655d2b6d63ed4e14e7525493f3e61027d49
Wilfred Denton <wdenton@Wilfreds-MacBook-Air.local>
2015-07-22 14:57:28 -0400

    added a default dispatcher. updated gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp')
 ,   sass = require('gulp-sass')
 ,   livereload = require('gulp-livereload')
+,   notify = require('gulp-notify')
 ,   watch = require('gulp-watch')
 ,   rimraf = require('gulp-rimraf')
+,   jshint = require('gulp-jshint')
+,   nodemon = require('gulp-nodemon')
 ,   browserify = require('browserify')
 ,   reactify = require('reactify')
 ,   source = require('vinyl-source-stream');
@@ -10,9 +13,17 @@ var gulp = require('gulp')
 gulp.task('browserify', function() {
   browserify('src/scripts/main.js')
     .transform('reactify')
-    .bundle()
+    .bundle().on('error', function(err) {console.log(err)})
     .pipe(source('main.js'))
-    .pipe(gulp.dest('dist/scripts'));
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(livereload());
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/scripts/**/*.*')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('cleanAssets', function() {
@@ -22,23 +33,27 @@ gulp.task('cleanAssets', function() {
 
 gulp.task('copy', ['cleanAssets'], function() {
   gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
   gulp.src('src/assets/**/*.*')
-    .pipe(gulp.dest('dist/assets'));
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(livereload());
 });
 
 gulp.task('sass', function() {
   gulp.src('src/stylesheets/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('dist/stylesheets'));
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('dist/stylesheets'))
+    .pipe(livereload());
 });
 
-gulp.task('default', ['browserify', 'sass', 'copy'], function() {
+gulp.task('default', function() {
   livereload.listen();
+  // nodemon({script: 'app.js', ignore: ['src/*', 'dist/*', 'gulpfile.js']});
   gulp.watch('src/scripts/**/*.js', ['browserify']);
   gulp.watch('src/index.html', ['copy']);
   watch('src/assets/**/*.*', function() {
     gulp.start('copy');
   });
-  gulp.watch('src/stylesheets/main.scss', ['sass']);
+  gulp.watch('src/stylesheets/**/*.scss', ['sass']);
 });
