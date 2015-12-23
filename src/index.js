fffd47f8d6efc58a084a40386b12ed7b0cc31c66
Wilfred Denton <dentonw3@gmail.com>
2015-12-23 15:53:42 -0500

    redux devtools are now working. they open in a new window so you have to allow popups on the dev page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import ReactDOM from  'react-dom'
+import { render } from  'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
 import Root from './root'
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById('root')
+)
+
+if (process.env.NODE_ENV !== 'production') {
+  const showDevTools = require('./showDevTools').default
+  showDevTools(store)
+}
+
--- src/root.js
+++ src/root.js
@@ -1,36 +1,24 @@
+require('../assets/stylesheets/style.css')
 import React, { Component } from 'react'
 import { Router, Route, IndexRoute } from 'react-router'
 import createHashHistory from 'history/lib/createHashHistory'
-import { Provider } from 'react-redux'
 import App from './components/app'
 import Home from './components/home'
 import NotFound from './components/not-found'
 import Test from './components/test'
-import configureStore from './store/configureStore'
-import DevTools from './DevTools'
 
 const history = createHashHistory()
-const store = configureStore()
-
-const routes = (
-	<Router history={history}>
-		<Route path="/" component={App}>
-			<IndexRoute component={Home} />
-			<Route path="test" component={Test} />
-		</Route>
-		<Route path="*" component={NotFound} />
-	</Router>
-)
 
 export default class Root extends Component {
   render() {
     return (
-			<Provider store={store}>
-				<div>
-					{routes}
-					<DevTools />
-				</div>
-			</Provider>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="test" component={Test} />
+        </Route>
+        <Route path="*" component={NotFound} />
+      </Router>
     )
   }
 }
