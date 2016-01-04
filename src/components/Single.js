fe95a6bc0599f49ce2496fdb8fd2e4ef5d36fe72
evankozliner <evankozliner@gmail.com>
2016-01-03 22:14:25 -0500

    Added marked

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,13 +1,18 @@
 import React, { PropTypes, Component } from 'react'
 import { Link } from 'react-router'
 import postData from '../postSampleData'
+import marked from 'marked'
 
 export default class Single extends Component {
 	static propType = {
 	}
+	rawMarkup() {
+		let md = postData.text
+		return {__html: marked('# I am using __markdown__.')}
+	}
 	render() {
 		return (
-      <p>{postData.text}</p>
+			<div dangerouslySetInnerHTML= {this.rawMarkup()} />
 		)
 	}
 }
